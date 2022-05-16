b568968681ed5d5963eb34af0492386d337ed0d0
Your Name <sarifulislaml8280@gmail.com>
2022-05-16 17:38:01 +0600

    working on booking api

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
-const port = process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express())
-
+app.use(cors());
+app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.boblx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -16,12 +15,23 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('doctor_portal').collection('services');
+        const bookingCollection = client.db('doctor_portal').collection('bookings');
+        // const bookingCollection = client.db('doctor_portal').collection('booking');
 
+        //all service get api
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        })
+
+        //booking post api
+        app.post('/booking', async (req, res) => {
+            const booking = req.body
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+            const result = await bookingCollection.insertOne(booking)
+            res.send(result)
         })
 
 
